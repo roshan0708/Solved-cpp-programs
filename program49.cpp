bda60183192b1ef3f80a3e470200218e6332f654
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-01 03:15:44 +0530

    450A - Jzzhu and Children
    Codeforces Round #257 (Div. 2)

---
+++ program49.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(i=a;i<b;i++)
+#define rep(i,n) f(i,0,n)
+#define fd(i,a,b) for(i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define ll long long
+#define ull unsigned long long
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define inf (1000*1000*1000+5)
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define pi acosl(-1)
+#define sqr(x) ((x)*(x))
+#define mod (1000*1000*1000+7)
+#define pdd pair<double,double>
+#define MEMS(x) memset(x,-1,sizeof(x))
+#define MEM(x) memset(x,0,sizeof(x))
+ll modPow(ll x,ll n,ll m)
+{
+    if(n==0)
+    return 1%m;
+    ll u = modPow(x,n/2,m);
+    u=u*u%m;
+    if(n%2==1)
+    u=u*x%m;
+    return u;
+}
+ll gcd(ll a,ll b)
+{
+    if(b==0)
+    return a;
+    return gcd(b,a%b);
+}
+ll seive[1000000];
+void seiveForm()
+{
+    ll i,j;
+    seive[0]=seive[1]=1;
+    f(i,2,100000)
+    {
+        if(seive[i]==0)
+            for(j=i*i;j<1000000;j+=i)
+                seive[j]=1;
+    }
+}
+bool isPerfectSquare(ll num)
+{
+    double n = sqrt(num);
+    return n==(ll)n?true:false;
+}
+bool isPrime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
+int main()
+{
+    ll i,n,m,t1=0,t2=0,maxi=-1;
+    cin>>n>>m;
+    ll arr[n];
+    rep(i,n)
+    {
+        cin>>arr[i];
+        if(arr[i]%m==0)
+            arr[i]/=m;
+        else
+            arr[i] = (arr[i]/m) + 1;
+        if(arr[i]>=maxi)
+        {
+            maxi=arr[i];
+            t1=i;
+        }
+    }
+    cout<<t1+1<<endl;
+    return 0;
+}
