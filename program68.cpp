a833929ca5e0b279d0388365bd90539a456bee38
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-16 13:54:52 +0530

    045B - Card Game for Three
    AtCoder Beginner Contest 045

---
+++ program68.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(i=a;i<b;i++)
+#define rep(i,n) f(i,0,n)
+#define fd(i,a,b) for(i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define ll long long
+#define ull unsigned long long
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define inf (1000*1000*1000+5)
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define pi acosl(-1)
+#define sqr(x) ((x)*(x))
+#define mod (1000*1000*1000+7)
+#define pdd pair<double,double>
+#define MEMS(x) memset(x,-1,sizeof(x))
+#define MEM(x) memset(x,0,sizeof(x))
+ll modPow(ll x,ll n,ll m)
+{
+    if(n==0)
+    return 1%m;
+    ll u = modPow(x,n/2,m);
+    u=u*u%m;
+    if(n%2==1)
+    u=u*x%m;
+    return u;
+}
+ll gcd(ll a,ll b)
+{
+    if(b==0)
+    return a;
+    return gcd(b,a%b);
+}
+ll seive[1000000];
+void seiveForm()
+{
+    ll i,j;
+    seive[0]=seive[1]=1;
+    f(i,2,100000)
+    {
+        if(seive[i]==0)
+            for(j=i*i;j<1000000;j+=i)
+                seive[j]=1;
+    }
+}
+bool isPerfectSquare(ll num)
+{
+    double n = sqrt(num);
+    return n==(ll)n?true:false;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s1,s2,s3;
+    ll i,j,k,l,temp=0;
+    cin>>s1>>s2>>s3;
+    char c;
+    i=0;
+    while(true)
+    {
+        if(c=='a' || i==0)
+        {
+            if(s1.length()==0)
+            {
+                cout<<"A\n";
+                return 0;
+            }
+            c=s1[0];
+            s1.erase(s1.begin());
+        }
+        else if(c=='b')
+        {
+            if(s2.length()==0)
+            {
+                cout<<"B\n";
+                return 0;
+            }
+            c=s2[0];
+            s2.erase(s2.begin());
+        }
+        else
+        {
+            if(s3.length()==0)
+            {
+                cout<<"C\n";
+                return 0;
+            }
+            c=s3[0];
+            s3.erase(s3.begin());
+        }
+        i++;
+    }
+    return 0;
+}
