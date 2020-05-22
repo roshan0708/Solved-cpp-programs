dab4deb7651f2b352c18a5126d013b892246f1f7
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-22 11:26:53 +0530

    287A - IQ Test
    Codeforces round #176(div 2)

---
+++ program74.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(i=a;i<b;i++)
+#define rep(i,n) f(i,0,n)
+#define fd(i,a,b) for(i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define ll long long
+#define ull unsigned long long
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define inf (1000*1000*1000+5)
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define pi acosl(-1)
+#define sqr(x) ((x)*(x))
+#define mod (1000*1000*1000+7)
+#define pdd pair<double,double>
+#define MEMS(x) memset(x,-1,sizeof(x))
+#define MEM(x) memset(x,0,sizeof(x))
+ll modPow(ll x,ll n,ll m)
+{
+    if(n==0)
+    return 1%m;
+    ll u = modPow(x,n/2,m);
+    u=u*u%m;
+    if(n%2==1)
+    u=u*x%m;
+    return u;
+}
+ll gcd(ll a,ll b)
+{
+    if(b==0)
+    return a;
+    return gcd(b,a%b);
+}
+ll seive[1000000];
+void seiveForm()
+{
+    ll i,j;
+    seive[0]=seive[1]=1;
+    f(i,2,100000)
+    {
+        if(seive[i]==0)
+            for(j=i*i;j<1000000;j+=i)
+                seive[j]=1;
+    }
+}
+bool isPerfectSquare(ll num)
+{
+    double n = sqrt(num);
+    return n==(ll)n?true:false;
+}
+ll countSetbits(ll num)
+{
+    ull temp=0;
+    while(num){
+        num=num&(num-1);
+        temp++;
+    }
+    return temp;
+}
+ll check1(char c)
+{
+    if(c=='#')
+        return 1;
+    return 0;
+}
+ll check2(char c)
+{
+    if(c=='.')
+        return 1;
+    return 0;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,i,j,temp=0,ans=0,t1=0,t2=0;
+    bool f=false;
+    char a[4][4];
+    rep(i,4){
+        rep(j,4){
+            cin>>a[i][j];
+        }
+    }
+    rep(i,3){
+        rep(j,3){
+            t1=check1(a[i][j])+check1(a[i+1][j])+check1(a[i][j+1])+check1(a[i+1][j+1]);
+            t2=check2(a[i][j])+check2(a[i+1][j])+check2(a[i][j+1])+check2(a[i+1][j+1]);
+            if((t1==3 && t2==1) || (t1==1 && t2==3) || (t1==4) || (t2==4))
+            {
+                f=true;
+                break;
+            }
+        }
+    }
+    if(f)
+        cout<<"YES\n";
+    else
+        cout<<"NO\n";
+    return 0;
+}
