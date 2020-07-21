26773029a05c680b94d12b05d4bf68d6007d3357
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-07-22 01:43:37 +0530

    Codeforces Round #513 (Div 1+Div 2)

---
+++ 1060A - Phone Numbers.cpp
@@ -0,0 +1,143 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(i=a;i<b;i++)
+#define rep(i,n) f(i,0,n)
+#define fd(i,a,b) for(i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define ll long long
+#define ull unsigned long long
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define inf (1000*1000*1000+5)
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define pi acosl(-1)
+#define sqr(x) ((x)*(x))
+#define mod (1000*1000*1000+7)
+#define pdd pair<double,double>
+#define MEMS(x) memset(x,-1,sizeof(x))
+#define MEM(x) memset(x,0,sizeof(x))
+template <typename T>
+string NumberToString(T pNumber)
+{
+     ostringstream oOStrStream;
+     oOStrStream << pNumber;
+     return oOStrStream.str();
+}
+ll StringToNumber(string s)
+{
+    stringstream geek(s);
+    ll x=0;
+    geek>>x;
+    return x;
+}
+ll modPow(ll x,ll n,ll m)
+{
+    if(n==0)
+    return 1%m;
+    ull u = modPow(x,n/2,m);
+    u=u*u%m;
+    if(n%2==1)
+    u=u*x%m;
+    return u;
+}
+ll gcd(ll a,ll b)
+{
+    if(b==0)
+    return a;
+    return gcd(b,a%b);
+}
+vl primes;
+void seiveForm(ll n)
+{
+    ll i,j;
+    vector<bool> isPrime(n+1,true);
+    isPrime[0]=false;
+    isPrime[1]=false;
+    f(i,2,n+1)
+    {
+        if(isPrime[i]==true)
+            for(j=i*i;j<=n+1;j+=i)
+                isPrime[j]=false;
+    }
+    rep(i,n+1)
+    {
+        if(isPrime[i]==true)
+            primes.pb(i);
+    }
+}
+bool isPerfectSquare(ll num)
+{
+    double n = sqrt(num);
+    return n==(ll)n?true:false;
+}
+ll countSetbits(ll num)
+{
+    ull temp=0;
+    while(num){
+        num=num&(num-1);
+        temp++;
+    }
+    return temp;
+}
+bool isPowerOfTwo(ll n)
+{
+   if(n==0)
+   return false;
+   return (ceil(log2(n)) == floor(log2(n)));
+}
+ll power(ll x, ll y)
+{
+    ll res = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = res*x;
+        y = y>>1;
+        x = x*x;
+    }
+    return res;
+}
+ll sumOfDigits(ll n)
+{
+    int sum;
+    for (sum = 0; n > 0; sum += n % 10, n /= 10);
+    return sum;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll i,j,k,ans,t,n;
+    string s;
+    cin>>n>>s;
+    ll a,b,temp,maxi=-1,pos;
+    bool f=false;
+    map<char,ll>mp;
+    rep(i,n)
+    {
+        mp[s[i]]++;
+    }
+    if(n<11 || mp['8']==0)
+        cout<<"0\n";
+    else
+    {
+        a=mp['8'];
+        b=n-a;
+        temp=n/11;
+        if(a>=temp)
+            cout<<temp<<"\n";
+        else
+            cout<<a<<"\n";
+    }
+    return 0;
+}
